53d3e8fdfbcd4272c0a19f0063dde9946379e576
enrique palomino <octepc.97@gmail.com>
2023-06-03 15:55:23 -0500

    added styles to the landing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,28 +1,73 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
-export const Landing = () => {
+const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobify" className="logo" />
       </nav>
-      <div class="container page">
+      <div className="container page">
         {/* info */}
-        <div class="info">
+        <div className="info">
           <h1>
             Job <span>tracking</span>{" "}
           </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            consectetur nobis suscipit sit beatae in pariatur quos qui
+            consequuntur. Nisi recusandae deleniti veritatis consequatur alias
+            numquam cupiditate iusto libero quae.
+          </p>
+          <button className="btn btn-hero">Login/register</button>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          consectetur nobis suscipit sit beatae in pariatur quos qui
-          consequuntur. Nisi recusandae deleniti veritatis consequatur alias
-          numquam cupiditate iusto libero quae.
-        </p>
-        <button className="btn btn-hero">Login/register</button>
+        <img src={main} alt="job hunt" className="img main-img" />
       </div>
-      <img src={main} alt="job hunt" className="img main-img" />
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
