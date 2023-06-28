9ae432839f6b54fd08c9ddf37e1277de70465cbc
enrique palomino <octepc.97@gmail.com>
2023-06-28 13:17:05 -0500

    feat: :sparkles: Made the Mongoose conection successfully

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import connectDB from "./db/connect.js";
 
 const app = express();
-
+dotenv.config();
 // middleware
 
 app.get("/", (req, res) => {
-  throw new Error("error");
+  // throw new Error("error");
   res.send("Welome!");
 });
 
@@ -16,4 +19,13 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
