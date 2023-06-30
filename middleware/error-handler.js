2df59e3f76669b1d6644999a7372c4cce2f55e07
enrique palomino <octepc.97@gmail.com>
2023-06-30 10:06:42 -0500

    feat: :sparkles: Installed a library that handles the error messages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,12 @@
+import { StatusCodes } from "http-status-codes";
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: err });
+  const defaultError = {
+    statusCodes: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong, try again later",
+  };
+  res.status(defaultError.statusCodes).json({ msg: err });
 };
 
 export default errorHandlerMiddleware;
